896d9e26e22c43081744718252423e2d8f21a062
Hayley Hume-Merry <hayley@thundermaps.com>
2013-12-23 15:37:36 +1300

    Addition of docstrings

--- piracy_feed.py
+++ piracy_feed.py
@@ -1,3 +1,8 @@
+'''This module can be used to retrieve live NATO Piracy Data
+
+Author: Hayley Hume-Merry <hayley@thundermaps.com>
+Date: 23 December 2013'''
+
 import urllib.request
 import time
 import xml.etree.ElementTree as ET
@@ -5,12 +10,14 @@ import pytz, datetime
 
 class Piracy:
     def format_feed(self):
+        #Retrieves and formats the piracy data from NATO's RSS Feed
         feed_file = urllib.request.urlretrieve("http://www.shipping.nato.int/_layouts/listfeed.aspx?List=77c1e451-15fc-49db-a84e-1e8536ccc972&View=721a920c-538a-404e-838b-30635159e886", "piracy_feed.xml")
         tree = ET.parse("piracy_feed.xml")
         listings = []
         for alert in tree.iter(tag='item'):
             summary = alert.find('description').text.split('<div>')
             for i in summary:
+                #Extracts the data from a number of subheadings within the feed
                 if i.startswith('<b>Category:</b>'):
                     title = i.replace('<b>Category:</b>', '').replace('</div>', '').strip()
                     title = title + " - NATO Piracy Alerts"
@@ -24,7 +31,7 @@ class Piracy:
             format_date = self.format_datetime(date)
             alert_id = alert.find('guid').text.split('=')
             alert_id = alert_id[1]
-            
+            #Converts the data into JSON format for application use
             listing = {"occurred_on":format_date, 
                        "latitude":latitude, 
                        "longitude":longitude, 
@@ -37,6 +44,7 @@ class Piracy:
              
             
     def format_datetime(self, date):
+        #Re-formats the publish time/date into UTC format
             date_time = date.replace(' GMT', '').split()
             day = date_time[1]
             monthDict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}

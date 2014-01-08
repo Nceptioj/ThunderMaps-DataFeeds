2fac4ec3aa4c32e7419ed3e9083350d5764cc73f
Hayley Hume-Merry <hayley@thundermaps.com>
2014-01-09 09:25:16 +1300

    Format Fix

--- caribbean_feed.py
+++ caribbean_feed.py
@@ -1,3 +1,8 @@
+'''This module can be used to retrieve and format Caribbean Sea Bulletins.
+
+Author: Hayley Hume-Merry
+Date: 07 January 2014'''
+
 import urllib.request
 import pytz, datetime
 import time
@@ -5,10 +10,12 @@ import xml.etree.ElementTree as ET
 
 class Caribbean:
     def format_feed(self):
+        #Retrieves the RSS feed from the Pacific Region Headquarters site
         warning_file = urllib.request.urlretrieve('http://www.prh.noaa.gov/ptwc/feeds/ptwc_rss_caribe.xml', 'warning_feed.xml')
         tree = ET.parse('warning_feed.xml')
         listings = []
         for entry in tree.iter(tag='item'):
+            #Formats the incident information into relevant subheadings
             title = entry.find('title').text + ': Caribbean Sea Alert'
             date = entry.find('pubDate').text
             format_date = self.format_datetime(date)
@@ -17,7 +24,7 @@ class Caribbean:
             description = entry.find('description').text.title()
             description = description.replace('<Pre>', '').replace('</Pre>', '').strip()
             entry_id = entry[3].text
-            
+            #Converts the data into JSON format for application use
             listing = {"occurred_on":format_date, 
                        "latitude":latitude, 
                        "longitude":longitude, 
@@ -30,6 +37,7 @@ class Caribbean:
     
             
     def format_datetime(self, date):
+        #convert date and time format to UTC
         date_time = date.split()
         day = date_time[0]
         monthDict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}

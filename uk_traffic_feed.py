19115f2bf349afe50d1238564ef36e1a3f5411aa
Hayley Hume-Merry <hayley@thundermaps.com>
2014-01-09 09:26:32 +1300

    UK traffic incidents

---
+++ uk_traffic_feed.py
@@ -0,0 +1,47 @@
+import urllib.request
+import pytz, datetime
+import time
+import xml.etree.ElementTree as ET
+
+class Incidents:
+    def format_feed(self):
+        traffic_feed = urllib.request.urlretrieve('http://hatrafficinfo.dft.gov.uk/feeds/rss/AllEvents.xml', 'incident_feed.xml')
+        tree = ET.parse('incident_feed.xml')
+        listings = []
+        for item in tree.iter(tag='item'):
+            incident_name = item.find('title').text
+            unique_id = item.find('reference').text
+            date = item.find('pubDate').text
+            format_date = self.format_datetime(date)
+            latitude = item.find('latitude').text
+            longitude = item.find('longitude').text
+            description = item.find('description').text
+            #format each parameter into a dictionary
+            listing = {"occurred_on":format_date, 
+                        "latitude":latitude, 
+                        "longitude":longitude, 
+                        "description":description,
+                        "category_name":incident_name,
+                        "source_id":unique_id}
+            #create a list of dictionaries
+            listings.append(listing)
+        return listings
+    
+            
+    def format_datetime(self, date):
+            #convert date and time format from GMT to UTC
+            date_time = date.split()
+            day = date_time[1]
+            monthDict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
+            month = date_time[2]
+            if month in monthDict:
+                month = monthDict[month]
+            year = date_time[3]
+            date_time = str(year) + '-' + str(month) + '-' + str(day) + ' ' + str(date_time[4])
+            
+            local = pytz.timezone("GMT")
+            naive = datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+            local_dt = local.localize(naive, is_dst = None)
+            utc_dt = local_dt.astimezone(pytz.utc)
+            utc_dt = str(utc_dt)
+            return utc_dt
